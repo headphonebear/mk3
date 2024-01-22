7bfdf22ae53b01e713e2c97b8ae92083e40b71bf
headphonebear <s.tefan.bachmann@gmail.com>
2024-01-22 19:07:47 +0100

    First draft of a class

--- listcreate.py
+++ listcreate.py
@@ -1,13 +1,21 @@
 import os
 import config
 
-# Walk through the directory and its subdirectories
+class Filelist:
+    mk3_source = ''
+    def create_filelist(self):
+        mk3_tree = os.walk(self.mk3_source)
+        filelist = []
+        for path, directories, files in mk3_tree:
+            directories.sort()
+            files = sorted(files)
+            for file in files:
+                element = path, file
+                filelist.append(element)
+        print(filelist[23456])
+    def Filelist():
+        pass
 
-mk3_tree = os.walk(config.mk3_source)
-
-for path, directories, files in mk3_tree:
-    directories.sort()
-    files = sorted(files)
-    for file in files:
-        print(path, file)
-
+myfilelist = Filelist()
+myfilelist.mk3_source = config.mk3_source
+myfilelist.create_filelist()
