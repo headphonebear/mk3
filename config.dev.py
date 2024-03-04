f27c1ed9f2f341d0d11d7af5b9a2dc61ee3c32a1
headphonebear <s.tefan.bachmann@gmail.com>
2024-03-04 13:54:17 +0100

    Took out useless extra-list, code much shorter. Removed testcode.

--- config.dev.py
+++ config.dev.py
@@ -1,4 +1,2 @@
 # path to root folder with flac files / music collection
 mk3_source = '/path-to-flac-root/'
-
-
--- listcreate.py
+++ listcreate.py
@@ -3,41 +3,21 @@ import os
 import redis
 import config
 
-class Filelist:
+class WorkerQueue:
     mk3_source = ''
-    def create_filelist(self):
+    name = ''
+    def create_queue(self):
         mk3_tree = os.walk(self.mk3_source)
-        filelist = []
         for path, directories, files in mk3_tree:
             directories.sort()
             files = sorted(files)
             for file in files:
-                element = path, file
-                filelist.append(element)
-        return(filelist)
-
-class Workerqueue:
-    filelist = ''
-    name = ''
-    def create_queue(self):
-        for line in range(len(self.filelist)):
-            print(self.filelist[line])
-            jsondata = json.dumps(self.filelist[line])
-            redis.rpush(self.name,jsondata)
+                redis.rpush(self.name, json.dumps((path, file)))
         return()
-
 
 redis = redis.Redis()
 
-myfilelist = Filelist()
-myfilelist.mk3_source = config.mk3_source
-testfilelist = myfilelist.create_filelist()
-
-myWorkerqueue = Workerqueue()
-myWorkerqueue.filelist = testfilelist
-myWorkerqueue.name = 'firstqueue'
-myWorkerqueue.create_queue()
-
-print (redis.lpop(myWorkerqueue.name))
-print (redis.lpop(myWorkerqueue.name))
-print (redis.lpop(myWorkerqueue.name))
+myworkerqueue = WorkerQueue()
+myworkerqueue.mk3_source = config.mk3_source
+myworkerqueue.name = 'newqueue'
+myworkerqueue.create_queue()
