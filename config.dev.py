a8b6fff0a3c41cfb399920c2910047ca7a943884
headphonebear <s.tefan.bachmann@gmail.com>
2024-03-05 15:48:20 +0100

    config updated

--- config.dev.py
+++ config.dev.py
@@ -1,2 +1,8 @@
 # path to root folder with flac files / music collection
 mk3_source = '/path-to-flac-root/'
+mp3_out = '/path-to-mp3/'
+taglist = ['ORIGINALDATE', 'MUSICBRAINZ_ALBUMID',
+        'MUSICBRAINZ_ALBUMARTISTID', 'ALBUMARTIST', 'ALBUMARTISTSORT',
+        'ALBUM', 'MUSICBRAINZ_TRACKID',
+        'MUSICBRAINZ_ARTISTID', 'ARTIST', 'ARTISTSORT',
+        'TITLE']
