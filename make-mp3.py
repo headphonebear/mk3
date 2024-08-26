40e1cd1edb496a592b1974be41e0a29904285d39
headphonebear <s.tefan.bachmann@gmail.com>
2024-08-26 15:31:01 +0200

    Minor fixes, style fixes.

--- make-mp3.py
+++ make-mp3.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
 
 import config
-import mk3lib
+import mk3lib.worker_queue
+import mk3lib.mk3_compiler
 import os
 
 up_counter = 0
 new_counter = 0
 update = True
-myworkerqueue = mk3lib.WorkerQueue(config.queue,config.mk3_source,"\.flac$")
+my_worker_queue = mk3lib.worker_queue.WorkerQueue(config.queue, config.mk3_source, "\.flac$")
 
-while (True):
-    myresult = myworkerqueue.get_next()
-    if myresult != 'Done':
-        mynewmp3 = mk3lib.Mp3Compiler(config.mk3_source, config.mp3_out, myresult[0], myresult[1], False,"320k")
-        if not os.path.exists(mynewmp3.mp3_new_path + mynewmp3.in_path):
-            os.makedirs(mynewmp3.mp3_new_path + mynewmp3.in_path)
-        if not os.path.isfile(mynewmp3.mp3_fullpath):
-            mynewmp3.compile()
-            mynewmp3.copy_tags()
-            mynewmp3.add_picture()
+while True:
+    my_result = my_worker_queue.get_next()
+    if my_result != 'Done':
+        my_new_mp3 = mk3lib.mk3_compiler.Mp3Compiler(config.mk3_source, config.mp3_out, my_result[0], my_result[1], False, "320k")
+        if not os.path.exists(my_new_mp3.mp3_new_path + my_new_mp3.in_path):
+            os.makedirs(my_new_mp3.mp3_new_path + my_new_mp3.in_path)
+        if not os.path.isfile(my_new_mp3.mp3_fullpath):
+            my_new_mp3.compile()
+            my_new_mp3.copy_tags()
+            my_new_mp3.add_picture()
             new_counter = new_counter + 1
         else:
-            if update == True:
-                mynewmp3.copy_tags()
-                mynewmp3.add_picture()
+            if update:
+                my_new_mp3.copy_tags()
+                my_new_mp3.add_picture()
             up_counter = up_counter + 1
     else:
         break
